23e79e173d8399253ca27ac17eb41d74daad07f8
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-01-31 06:17:11 +0100

    Add .gitignore && .npmignore

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var path = require('path')
 
 var posthtml = require('posthtml')
 
-module.exports = function ('html', (path, options, callback) => {
+module.exports = function (path, options, callback) {
   fs.readFile(path, function (err, content) {
-    if (err) return callback(new Error(err));
+    if (err) return callback(new Error(err))
     var plugins = options
     var posthtml = posthtml(plugins)
       .process(content.toString())
       .then((result) => {
         console.log(result.html)
-        return callback(null, result.html);
+        return callback(null, result.html)
       })
-  });
-}))
+  })
+}
