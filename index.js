80c1adb3a99257a67b7668f7af2a6954dc123f67
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-01-31 06:00:42 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+var path = require('path')
+
+var posthtml = require('posthtml')
+
+module.exports = function ('html', (path, options, callback) => {
+  fs.readFile(path, function (err, content) {
+    if (err) return callback(new Error(err));
+    var plugins = options
+    var posthtml = posthtml(plugins)
+      .process(content.toString())
+      .then((result) => {
+        console.log(result.html)
+        return callback(null, result.html);
+      })
+  });
+}))
