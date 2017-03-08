70d4c42eead7546297c0262801f92c7585b4aa41
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-03-08 01:33:19 +0100

    test(index): add tests

---
+++ test/index.test.js
@@ -0,0 +1,93 @@
+// ------------------------------------
+// #EXPRESS - POSTHTML - TEST
+// ------------------------------------
+
+'use strict'
+const path = require('path')
+
+const express = require('express')
+const supertest = require('supertest')
+
+const posthtml = require('..')
+
+
+test('Pug', () => {
+  const app = express()
+
+  const plugins = [
+    require('posthtml-bem')(),
+    require('posthtml-expressions')({ locals: { foo: 'bar' } })
+  ]
+  const options = { parser: require('posthtml-pug')() }
+
+  app.engine('pug', posthtml)
+
+  app.set('views', path.resolve(__dirname, 'fixtures'))
+  app.set('view options', { plugins: plugins, options: options })
+
+  app.get('/', (req, res) => res.render('index.pug'))
+
+  return supertest(app).get('/').expect(200).then((res) => {
+    expect(res.text.trim()).toEqual('<div>bar</div>')
+  })
+})
+
+test('HTML', () => {
+  const app = express()
+
+  const plugins = [
+    require('posthtml-bem')(),
+    require('posthtml-expressions')({ locals: { foo: 'bar' } })
+  ]
+
+  app.engine('html', posthtml)
+
+  app.set('views', path.resolve(__dirname, 'fixtures'))
+  app.set('view options', { plugins: plugins })
+
+  app.get('/', (req, res) => res.render('index.html'))
+
+  return supertest(app).get('/').expect(200).then((res) => {
+    expect(res.text.trim()).toEqual('<div>bar</div>')
+  })
+})
+
+test('SSML', () => {
+  const app = express()
+
+  const plugins = [
+    require('posthtml-bem')(),
+    require('posthtml-expressions')({ locals: { foo: 'bar' } })
+  ]
+  const options = { parser: require('posthtml-sugarml')() }
+
+  app.engine('ssml', posthtml)
+
+  app.set('views', path.resolve(__dirname, 'fixtures'))
+  app.set('view options', { plugins: plugins, options: options })
+
+  app.get('/', (req, res) => res.render('index.ssml'))
+
+  return supertest(app).get('/').expect(200).then((res) => {
+    expect(res.text.trim()).toEqual('<div>bar</div>')
+  })
+})
+
+test.skip('Error', () => {
+  const app = express()
+
+  const plugins = [
+    require('posthtml-bem')(),
+    require('posthtml-expressions')({ locals: { foo: 'bar' } })
+  ]
+  const options = { parser: require('posthtml-sugarml')() }
+
+  app.engine('ssml', posthtml)
+
+  app.set('views', path.resolve(__dirname, 'fixtures'))
+  app.set('view options', { plugins: plugins, options: options })
+
+  app.get('/', (req, res) => res.render('error.ssml'))
+
+  return supertest(app).get('/').then((res) => expect(res.status).toEqual(500))
+})
